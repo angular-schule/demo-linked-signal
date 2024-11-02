bc7eb0546f6de4908b1cb98de54163c6e47d12c0
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-11-02 18:02:41 +0100

    + first example (BookListComponent)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { DashboardComponent } from './books/dashboard/dashboard.component';
+import { BookListComponent } from './books/book-list/book-list.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [DashboardComponent]
+  imports: [DashboardComponent, BookListComponent]
 })
 export class AppComponent {
   title = 'Book Rating';
---
+++ src/app/books/book-list/book-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, linkedSignal, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-book-list',
+  template: `
+    <p>First book in list: {{ firstBook() }}</p>
+    <button (click)="changeBookList()">Change Book List</button>
+  `,
+})
+export class BookListComponent {
+  books = signal(['Angular', 'React', 'Vue']);
+
+  firstBook = linkedSignal({
+    source: this.books,
+    computation: books => books[0]
+  });
+
+  overrideFirstBook() {
+    // Manually updating `firstBook`, which now returns 'jQuery'
+    this.firstBook.set('jQuery');
+  }
+
+  changeBookList() {
+    // Changing `books` causes `firstBook` to reset, now returning 'Next.js'
+    this.books.set(['Next.js', 'Svelte', 'Nuxt']);
+  }
+}
